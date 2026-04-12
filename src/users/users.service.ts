fa8274590eabe53fb0eec02755a428670cb72c56
Shella <61147190+ShellaDeLaCruz@users.noreply.github.com>
2026-04-12 13:11:53 +1000

    Update Prisma schema and ERD alignment

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+
+    constructor(private prisma: PrismaService) {
+    }
+    //Finds user by email
+    findByEmail = (email: string) => this.prisma.user.findUnique({ where: { email } });
+    private isValidPassword(password: string): boolean {
+        const minLength = password.length >= 6;
+        const hasNumber = /[0-9]/.test(password);
+        const hasSpecialChar = /[!@#$%*?]/.test(password);
+        return minLength && hasNumber && hasSpecialChar;
+    }
+    async create(email: string, password: string) {
+        if (!this.isValidPassword(password)) {
+            throw new Error('Password must be at least 6 charachters and include at least 1 number and 1 special charachter (!@#$%*?)');
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        return await this.prisma.user.create({ data: { email, password: hashedPassword } });
+    }
+}
