25370b7875930daed06f7c3b2ec3683a4e389d61
fortopes77 <azelia.clarkson1@gmail.com>
2026-07-01 19:56:55 +1000

    Fix build issues

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,7 +5,8 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     const adapter = new PrismaMariaDb({
       host: '127.0.0.1',
