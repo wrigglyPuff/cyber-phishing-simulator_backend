811491ca55e1bf6ec16350c3fd841707adcf203e
Shella <61147190+ShellaDeLaCruz@users.noreply.github.com>
2026-08-13 00:11:14 +1000

    Add results module

--- src/results/results.module.ts
+++ src/results/results.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResultsController } from './results.controller';
 import { ResultsService } from './results.service';
+import { PrismaModule } from '../prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ResultsController],
-  providers: [ResultsService]
+  providers: [ResultsService],
 })
-export class ResultsModule {}
+export class ResultsModule { }
